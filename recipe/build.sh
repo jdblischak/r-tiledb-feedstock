d838aab8258287a2810e7823c3f47dae9343fa8e
John Blischak <jdblischak@gmail.com>
2024-06-11 11:32:08 -0400

    Build without the wrapper scripts on osx-64

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,16 +11,16 @@ echo
 echo
 echo
 
-if [[ $target_platform  == osx-64 ]]; then
-  export NN_CXX_ORIG=$CXX
-  export NN_CC_ORIG=$CC
-  export CXX=$RECIPE_DIR/cxx_wrap.sh
-  export CC=$RECIPE_DIR/cc_wrap.sh
-  mkdir -p ~/.R
-  echo CC=$RECIPE_DIR/cc_wrap.sh > ~/.R/Makevars
-  echo CXX=$RECIPE_DIR/cxx_wrap.sh >> ~/.R/Makevars
-  echo CXX17=$RECIPE_DIR/cxx_wrap.sh >> ~/.R/Makevars
-fi
+# if [[ $target_platform  == osx-64 ]]; then
+#   export NN_CXX_ORIG=$CXX
+#   export NN_CC_ORIG=$CC
+#   export CXX=$RECIPE_DIR/cxx_wrap.sh
+#   export CC=$RECIPE_DIR/cc_wrap.sh
+#   mkdir -p ~/.R
+#   echo CC=$RECIPE_DIR/cc_wrap.sh > ~/.R/Makevars
+#   echo CXX=$RECIPE_DIR/cxx_wrap.sh >> ~/.R/Makevars
+#   echo CXX17=$RECIPE_DIR/cxx_wrap.sh >> ~/.R/Makevars
+# fi
 
 ## Build a tarball and install from the tarball is the officially blessed way
 ## It has also respects the .Rbuildignore file allowing us to fine-tune
